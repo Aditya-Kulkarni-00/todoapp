53d05ef70d8cdb1b45e4b1c330c6fadc29f3e768
Aditya-Kulkarni-00 <44834398+Aditya-Kulkarni-00@users.noreply.github.com>
2019-11-27 12:02:38 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
 
-mongoose.connect('mongodb://localhost/todo-api',{useNewUrlParser:true, useUnifiedTopology :true,useFindAndModify:false});
+mongoose.connect('mongodb+srv://Admin:1EXbwARgdpOb72ti@min-system-api-4jry3.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology :true,useFindAndModify:false});
 mongoose.Promise = Promise;
 
 
